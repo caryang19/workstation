d3adf8fdad3e3abb8f126b6d59c4d6000b7d62ef
Olumide Olamilekan <51947356+caryang19@users.noreply.github.com>
2021-05-27 07:37:14 +0100

    new

--- workstation/settings.py
+++ workstation/settings.py
@@ -18,6 +18,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+
 STATIC_FILES = [
     os.path.join(BASE_DIR, 'static')
 
